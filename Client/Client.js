a61d98f4fbb085de0e6f477cfdad7073e614ca53
Rafael Da Cunha Garcia <39766039+Rafiibro@users.noreply.github.com>
2019-06-02 22:32:26 +0200

    SALUT

--- Client/Client.js
+++ Client/Client.js
@@ -1,27 +1,106 @@
-var http = require('http');
+var request = require("request-promise");
+var port = process.argv[2] || 8080;
+var ip = process.argv[3] || "localhost";
+var repFormat = process.argv[4] || "json";
+var reqFormat = process.argv[5] || "json";
 
-var postData = "time";
+var headers = {
+    'Accept': 'text/' + repFormat,
+}
 
 var options = {
-    hostname: "localhost",
-    port: 3000,
-    method: "POST",
-    headers: {
-
-    }
+    method: 'GET',
+    uri: 'http://' + ip + ':' + port,
+    headers: headers
 };
-var body = '';
-var request = http.request(options, function (res) {
-    if (res.method === 'POST') {
-    }
-    res.on('data', function (data) {
-        body += data;
+
+request(options)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
     });
 
-    res.on('end', function () {
-        console.log(body);
+
+var headers = {
+    'Accept': 'text/' + repFormat,
+}
+
+var options = {
+    method: 'POST',
+    uri: 'http://' + ip + ':' + port,
+    headers: headers,
+    body: {
+        hours: '4',
+        minutes: '20',
+        seconds: '0'
+    }
+};
+
+if (reqFormat == 'json') {
+    options.json = true;
+} else {
+    options.body = "<root><hours>4</hours><minutes>20</minutes><seconds>0</seconds></root>"
+}
+
+request(options)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+var headers = {
+    'Accept': 'text/' + repFormat,
+}
+
+var options = {
+    method: 'GET',
+    uri: 'http://' + ip + ':' + port,
+    headers: headers
+};
+
+request(options)
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+/*
+var http = require('http');
+
+var post_req  = null,
+    post_data = '{"login":"toto","password":"okay","duration":"9999"}';
+
+var post_options = {
+    hostname: '192.168.1.1',
+    port    : '8080',
+    path    : '/web/authenticate',
+    method  : 'POST',
+    headers : {
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Content-Length': post_data.length
+    }
+};
+
+post_req = http.request(post_options, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('Response: ', chunk);
     });
 });
 
-request.write(postData);
-request.end();
+post_req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+});
+
+post_req.write(post_data);
+post_req.end();
+ */
--- Server/Server.js
+++ Server/Server.js
@@ -1,30 +1,87 @@
 var http = require("http");
-var moment = require("moment");
+var xml2js = require('xml2js');
+var port = process.argv[2] || 8080;
+var date = new Date();
 
-var server = http.createServer().listen(3000);
+console.log('Server run on port ' + port);
 
-console.log("Jecoute sur le port 3000");
+setInterval(incrementDate, 1000);
 
-server.on('request', function (req, res) {
-
+http.createServer(function (req, res) {
+    const { headers } = req;
     if (req.method === 'POST') {
-        var body = '';
-    }
+        var body = "";
 
-    req.on('data', function (data) {
-        body += data;
-    });
+        req.on('data', function (data) {
+            body += data;
+        });
+        req.on('end', () => {
+            if(headers['content-type'] == 'application/json'){
+                body = JSON.parse(body);
+                date.setHours(body.hours);
+                date.setMinutes(body.minutes);
+                date.setSeconds(body.seconds);
+            } else {
+                xml2js.parseString(body, function (err, result) {
+                    body = JSON.parse(JSON.stringify(result));
+                });
+                date.setHours(body.root.hours);
+                date.setMinutes(body.root.minutes);
+                date.setSeconds(body.root.seconds);
+            }
 
-    req.on('end', function () {
-        console.log(body);
-        var post = "";
-        if (body == "time") {
-            post = new Date();
-            res.write(post.toDateString());
-        } else {
-            res.write("bite");
+        });
 
-        }
+        console.log("ok");
+        var send = {reponse: "ok"};
+        res.write(reponseFormat(headers['accept'], send));
         res.end();
-    });
-});
+
+    } else if (req.method === 'GET') {
+        console.log(req.method);
+        var hour = date.getHours();
+        hour = (hour < 10 ? "0" : "") + hour;
+
+        var min = date.getMinutes();
+        min = (min < 10 ? "0" : "") + min;
+
+        var sec = date.getSeconds();
+        sec = (sec < 10 ? "0" : "") + sec;
+
+        var send = {hour: hour + ":" + min + ":" + sec};
+        console.log(send);
+
+        res.write(reponseFormat(headers['accept'], send));
+        res.end();
+    }
+}).listen(port);
+
+function reponseFormat(format, text) {
+    if(format == 'text/json'){
+        return reponseJSON(text);
+    } else if(format == 'text/xml'){
+        return reponseXML(text);
+    } else {
+        return reponseHTML(text);
+    }
+}
+
+function reponseJSON(text) {
+    return JSON.stringify(text);
+}
+
+function reponseHTML(text) {
+    if(text.reponse == 'ok'){
+        return '<html><body><p>' + text.reponse + '</p></body></html>';
+    }
+    return '<html><body><p>' + text.hour + '</p></body></html>';
+}
+
+function reponseXML(text) {
+    var builder = new xml2js.Builder();
+    return builder.buildObject(text);
+}
+
+function incrementDate(){
+    date = new Date(date.getTime() + 1000);
+}
